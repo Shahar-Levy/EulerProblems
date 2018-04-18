c0d2a80dc2b08adf1fc1bfc6ec9e279dc858cb64
Shahar Levy <shaharlevy71@gmail.com>
2018-04-18 14:54:07 -0400

    Completed the first three problems

---
+++ src/com/ShaharLevy/ProblemThree.java
@@ -0,0 +1,25 @@
+package com.ShaharLevy;
+
+public class ProblemThree {
+    //The prime factors of 13195 are 5, 7, 13 and 29.
+    //What is the largest prime factor of the number 600851475143?
+
+    public static void main(String[] args){
+        System.out.println(largestPrime(600851475143L));
+    }
+
+    public static long largestPrime(long num){
+        long result = 0L;
+        long squareRootOfNum = Math.round(Math.sqrt(num));
+        System.out.println(squareRootOfNum);
+        for(long i = squareRootOfNum; i > 0; i--){
+            if (num % i == 0){
+                System.out.println (i);
+
+//                return (num/i);
+            }
+        }
+        return -1;
+    }
+
+}
