dc4e33c6843edaedc80ba4e905d696f1f1f0d36d
Shahar Levy <shaharlevy71@gmail.com>
2018-04-24 13:25:14 -0400

    Finished problem 5

---
+++ src/com/ShaharLevy/ProblemFive.java
@@ -0,0 +1,29 @@
+package com.ShaharLevy;
+
+public class ProblemFive {
+
+    //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+    public static void main(String[] args){
+        System.out.println(divisibleByFirst20());
+    }
+
+    public static long divisibleByFirst20(){
+        long result = 1;
+        boolean numFound = false;
+
+        while(!numFound){
+            for (int i = 1; i <= 20; i++){
+                if(i == 20){
+                    return result;
+                }
+                if(result % i != 0){
+                    break;
+                }
+            }
+            result ++;
+        }
+        return -1;
+    }
+}
---
+++ src/com/ShaharLevy/ProblemSix.java
@@ -0,0 +1,19 @@
+package com.ShaharLevy;
+
+public class ProblemSix {
+
+    //the difference between the sum of the squares of the first ten
+    //natural numbers and the square of the sum is 3025 − 385 = 2640.
+    //Find the difference between the sum of the squares of the first
+    //one hundred natural numbers and the square of the sum.
+
+    public static void main(String[] args){}
+
+    public static int eulerProblemSix(){
+        for(int i = 0; i <= 100; i++){
+
+        }
+    }
+
+
+}
