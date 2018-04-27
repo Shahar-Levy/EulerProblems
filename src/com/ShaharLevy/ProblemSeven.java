d8aee832cbd8144ccf52694d29b2d57e2e253b3a
Shahar Levy <shaharlevy71@gmail.com>
2018-04-27 12:41:10 -0400

    finished problems 6 and 7. 8 is in progress.

---
+++ src/com/ShaharLevy/ProblemSeven.java
@@ -0,0 +1,42 @@
+package com.ShaharLevy;
+
+import java.util.ArrayList;
+
+public class ProblemSeven {
+    //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+    //we can see that the 6th prime is 13.
+    //What is the 10 001st prime number?
+
+    static ArrayList<Long> primeList = new ArrayList<Long>(){{
+        add(2L);
+    }};
+
+    public static void main (String[] args){
+
+        System.out.println(nthPrimeNum(10001));
+    }
+
+    public static boolean isPrime(long num){
+        for (Long prime : primeList) {
+            if (num % prime == 0){
+                return false;
+            }
+        }
+        primeList.add(num);
+        return true;
+    }
+
+    public static long nthPrimeNum(long num){
+        long primeCount = 1; //2 is already added
+        long index = 2;
+
+        while (primeCount < num){
+            if(isPrime(index)){
+                primeCount++;
+            }
+            index++;
+        }
+        return index-1;
+    }
+
+}
--- src/com/ShaharLevy/ProblemSix.java
+++ src/com/ShaharLevy/ProblemSix.java
@@ -7,12 +7,22 @@ public class ProblemSix {
     //Find the difference between the sum of the squares of the first
     //one hundred natural numbers and the square of the sum.
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.println(eulerProblemSix());
+    }
 
     public static int eulerProblemSix(){
-        for(int i = 0; i <= 100; i++){
+        int sumSquares = 0;
+        int sum = 0;
 
+        for(int i = 0; i <= 100; i++){
+            sumSquares += i*i;
+            sum += i;
         }
+        int squareSums = sum * sum;
+        System.out.println("sum of squares: " + sumSquares);
+        System.out.println("square of sums: " + squareSums);
+        return squareSums - sumSquares;
     }
 
 
